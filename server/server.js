4311c683c86c5e311fd60f92530aeebe5ad22fe8
Norwood91 <robnorwood91@gmail.com>
2025-06-11 18:30:22 -0700

    Set up the .env file. Refactored the server.js file. Created two different scripts to set the NODE_ENV environment correctly. Created a test route to make sure the server is running correctly and it is.

--- server/server.js
+++ server/server.js
@@ -1,4 +1,35 @@
-import dotenv from 'dotenv';
-dotenv.config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
-mongoose.connect(process.env.MONGODB_URI);
+if (process.env.NODE_ENV !== 'production') {
+    dotenv.config();
+}
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get('/', (req, res) => {
+    res.send('Server is running!');
+})
+
+// Connect to MongoDB
+const startServer = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URI);
+        console.log('MongoDB Connected Successfully!');
+        const PORT = process.env.PORT || 5000;
+        app.listen(PORT, () => {
+            console.log('Server running on port: ', PORT);
+        });
+    } catch (err) {
+        console.error('Failed to connect to MongoDB: ', err.message);
+    }
+}
+
+startServer();
