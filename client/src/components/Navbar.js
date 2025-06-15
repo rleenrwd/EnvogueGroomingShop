991542db1406c319a75b5e6c0c6afaf8c092a5b8
Norwood91 <robnorwood91@gmail.com>
2025-06-14 21:16:03 -0700

    Added two images for the services page. Rearranged the LI's on the navbar. Imported fonts to the global styles.css file. Provided the logic to fetch the services in the Service.js file.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,34 +1,50 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import BrandLogo from '../assets/navbarAssets/evLogo.png'
 
 const Navbar = () => {
     return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+    <nav id='mainNav' className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
       <div className="container">
-        <Link className="navbar-brand" to="/">EnVogue Grooming</Link>
+        <Link className="navbar-brand" to="/">
+        <img 
+        id='brandLogo'
+        src={BrandLogo} 
+        width='120px' 
+        alt='En Vogue Pet Grooming Brand Logo'  
+        className='img-fluid'
+        />
+        En Vogue Pet Grooming</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
+          <ul className="navbar-nav ms-auto text-center">
+
             <li className="nav-item">
               <Link className="nav-link" to="/">Home</Link>
             </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/services">Services</Link>
-            </li>
+
             <li className="nav-item">
               <Link className="nav-link" to="/about">About</Link>
             </li>
+
+            <li className="nav-item">
+              <Link className="nav-link" to="/services">Services</Link>
+            </li>
+
             <li className="nav-item">
               <Link className="nav-link" to="/contact">Contact</Link>
             </li>
+
             <li className="nav-item">
               <Link className="nav-link" to="/booking">Booking</Link>
             </li>
+
             <li className="nav-item">
               <Link className="nav-link" to="/testimonials">Testimonials</Link>
             </li>
+
             <li className="nav-item">
               <Link className="nav-link" to="/admin">Admin</Link>
             </li>
--- client/src/pages/Services.js
+++ client/src/pages/Services.js
@@ -1,14 +1,55 @@
 import Navbar from '../components/Navbar';
 import '../styles/Services.css';
+import React, { useEffect, useState} from 'react';
+import { getServices } from '../services/servicesAPI';
+import mainImg from '../assets/serviceAssets/dog_bath.jpg';
+
+
 
 const Services = ()  => {
+    const [services, setServices] = useState([]);
+
+    useEffect(() => {
+        const fetchServices = async () => {
+            try {
+                const data = await getServices();
+                setServices(data);
+            } catch (err) {
+                console.error('Error fetching services: ', err.message);
+            }
+        };
+        fetchServices();
+    }, []); // This empty array tells react when to run the code inside useEffect
+            // An empty array means, run the function only once at the beginning(first mounts)
+            // Without the array at the end, your code inside useEffect will run on every render and 
+            // That's not what we want when fetching data. So for api calls, use the [] empty array
+            // To avoid multiple unnecessary requests. 
+
+
+
     return (
         <>
-            <Navbar />
-            <div className="container-fluid text-center">
-                <h1>This is the services page.</h1>
+         <Navbar />
+         <div className='container-fluid px-0' id='mainImg'>
+
+            <img 
+            src={mainImg} 
+            className='img-fluid' 
+            alt='Dog getting a bath by groomer' />
+
+         </div>
+
+         <div className='container-fluid'>
+            <div className='row'>
+                <div className='col'>
+                    <h2 className='text-center pt-5'>Our Signature Services</h2>
+                </div>
+
+                <div className='col'>
+                    <h2 className='text-center pt-5'>Our Signature Services</h2>
+                </div>
             </div>
-        
+         </div>
         </>
     );
 };
