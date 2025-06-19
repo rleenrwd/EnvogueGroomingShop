e697ce3d5ff4eb3fa518d60ca7d959ef1d94ca82
Norwood91 <robnorwood91@gmail.com>
2025-06-19 03:29:45 -0700

    Added hero text to the homepage, styled it and made it responsive. Same with the navbar brand logo and brand text, it's now styled better on mobile and responsive.

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import BrandLogo from '../assets/navbarAssets/evLogo.png'
+import BrandLogo from '../assets/navbarAssets/transparent.png'
 import '../styles/Navbar.css';
 
 const Navbar = () => {
@@ -14,15 +14,23 @@ const Navbar = () => {
 
     <nav id='mainNav' className="navbar navbar-expand-lg navbar-light bg-light sticky-top">
       <div className="container">
+        <Link className='onSmallOnly' to="/services">
+          <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-envelope" viewBox="0 0 16 16">
+            <path d="M0 4a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2zm2-1a1 1 0 0 0-1 1v.217l7 4.2 7-4.2V4a1 1 0 0 0-1-1zm13 2.383-4.708 2.825L15 11.105zm-.034 6.876-5.64-3.471L8 9.583l-1.326-.795-5.64 3.47A1 1 0 0 0 2 13h12a1 1 0 0 0 .966-.741M1 11.105l4.708-2.897L1 5.383z"/>
+          </svg>
+        </Link>
+        
+
         <Link className="navbar-brand" to="/">
         <img 
         id='brandLogo'
         src={BrandLogo} 
-        width='120px' 
+        width='90px' 
         alt='En Vogue Pet Grooming Brand Logo'  
         className='img-fluid'
         />
-        <span id='shopName'>En Vogue Pet Grooming</span></Link>
+        <span id='shopName'> En Vogue Pet Grooming</span></Link>
+
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
           <span className="navbar-toggler-icon"></span>
         </button>
--- client/src/pages/Homepage.js
+++ client/src/pages/Homepage.js
@@ -1,17 +1,27 @@
 import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 import '../styles/Homepage.css';
+// import haircut from '../assets/aboutAssets/haircut.mp4'
 
 const Homepage = () => {
     return (
         <>
             <Navbar />
-            <div className="text-center py-5">
-                <h1 className='display-1'>Welcome to En Vogue Pet Grooming</h1>
+
+            <div className='text-center container-fluid'>
+                <h1  id='heroText'>En Vogue</h1>
                 <p>Professional grooming services to keep your pets looking and feeling their best.</p>
                 <button href="/services" variant="primary" size="lg">View our Services</button>
             </div>
-        
-        </>
+
+            {/* <div className="text-center py-2 container-fluid" id='headerContainer'>
+                <video className='bg-video' autoplay loop muted>
+                    <source src={haircut} type='video/mp4' />
+                </video>
+            </div> */}
+
+            <Footer />
+        </> 
     );
 };
 
